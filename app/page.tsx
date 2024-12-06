5fec5e789753da95c29fc9b331abd144e9cba322
levananh202ht <levananh202@evananh202gmail.com>
2024-12-06 10:02:13 +0700

    feat(next)

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,18 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import { neon } from '@neondatabase/serverless';
 
+export const experimental_ppr = true;
 export default function Page() {
+  async function create(formData: FormData) {
+    'use server';
+    // Connect to the Neon database
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const comment = formData.get('comment');
+    // Insert the comment from the form into the Postgres database
+    await sql('INSERT INTO comments (comment) VALUES ($1)', [comment]);
+  }
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
